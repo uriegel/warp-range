f9189747668c9fd92e6399bc328b2091c89a2457
uriegel <uriegel@hotmail.de>
2021-06-17 17:15:20 +0200

    Now ranges are functioning

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,8 +1,8 @@
-use std::cmp::min;
+use std::{cmp::min, io::SeekFrom, num::ParseIntError};
 
 use chrono::Utc;
 use hyper::StatusCode;
-use tokio::io::AsyncReadExt;
+use tokio::io::{AsyncReadExt, AsyncSeekExt};
 use async_stream::stream;
 use warp::{Filter, Rejection, Reply, fs::{
         File, dir
@@ -35,18 +35,16 @@ fn create_headers() -> HeaderMap {
     header_map
 }
 
-// fn get_range_params(range: &str)->Result<(u64, u64), Error> {
-//     let range: Vec<String> = range
-//         .replace("bytes=", "")
-//         .split("-")
-//         .filter_map(|n| if n.len() > 0 {Some(n.to_string())} else {None})
-//         .collect();
-//     let start = if let start = range[0] {start} else {0}
-
-//     }
-    
-//     let end = if range.len() > 0 {range[1]}  (size-1).to_string()
-// }
+fn get_range_params(range: &str, size: u64)->Result<(u64, u64), Error> {
+    let range: Vec<String> = range
+        .replace("bytes=", "")
+        .split("-")
+        .filter_map(|n| if n.len() > 0 {Some(n.to_string())} else {None})
+        .collect();
+    let start = range[0].parse::<u64>()?;
+    let end = if range.len() > 1 {range[1].parse::<u64>()?} else { size-1 };
+    Ok((start, end))
+}
 
 
 #[derive(Debug)]
@@ -55,6 +53,11 @@ pub struct Error {
 
 impl From<std::io::Error> for Error {
     fn from(err: std::io::Error) -> Self {
+        Error {}
+    }
+}
+impl From<ParseIntError> for Error {
+    fn from(err: ParseIntError) -> Self {
         Error {}
     }
 }
@@ -67,22 +70,29 @@ async fn internal_get_range(range_header: String, file: &str) -> Result<impl war
     let mut file = tokio::fs::File::open(file).await?;
     let metadata = file.metadata().await?;
     let size = metadata.len();
+    let (start_range, end_range) = get_range_params(&range_header, size - 1)?;
 
 
+
+    
+    println!("range_header: {}, start_range: {}, end_range: {}", range_header, start_range, end_range);
+    let byte_count = end_range - start_range + 1;
+    file.seek(SeekFrom::Start(start_range)).await?;
 
     let stream = stream! {
         let bufsize = 16384;
-        let cycles = size / bufsize as u64 + 1;
+        let cycles = byte_count / bufsize as u64 + 1;
         let mut sent_bytes: u64 = 0;
         for _ in 0..cycles {
-            let mut buffer: Vec<u8> = vec![0; min(size - sent_bytes, bufsize) as usize];
+            let mut buffer: Vec<u8> = vec![0; min(byte_count - sent_bytes, bufsize) as usize];
             match file.read_exact(&mut buffer).await {
                 Ok(res) => {
                     sent_bytes += res as u64;
-                    println!("Video stream: {}, {}, {}", res, buffer.len(), sent_bytes);
+                    //println!("Video stream: {}, {}, {}", res, buffer.len(), sent_bytes);
                     yield Ok(buffer) as Result<Vec<u8>, hyper::Error>;
                 },
                 Err(error) => {
+                    // TODO: panic!
                     println!("Could not get video stream: {:?}", error);
                     //let affe = Err(error) as Result<Vec<u8>, hyper::Error>;
                     // let affe = Err(error) as Result<Vec<u8>, std::io::Error>;
@@ -102,8 +112,8 @@ async fn internal_get_range(range_header: String, file: &str) -> Result<impl war
     let mut header_map = create_headers();
     header_map.insert("Content-Type", HeaderValue::from_str("video/mp4").unwrap());
     header_map.insert("Accept-Ranges", HeaderValue::from_str("bytes").unwrap());
-    header_map.insert("Content-Range", HeaderValue::from_str(&format!("bytes {}-{}/{}", 0, size - 1, size)).unwrap());
-    header_map.insert("Content-Length", HeaderValue::from(size));
+    header_map.insert("Content-Range", HeaderValue::from_str(&format!("bytes {}-{}/{}", start_range, end_range, size)).unwrap());
+    header_map.insert("Content-Length", HeaderValue::from(byte_count));
     headers.extend(header_map);
     Ok (response)
 }
